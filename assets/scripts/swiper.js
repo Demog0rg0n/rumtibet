a4577f29f5d48cf787664a3f2d80a7699d4b26dd
Ivan <iva.kot1331@gmail.com>
2024-02-09 13:25:46 +0300

    Адаптив

--- assets/scripts/swiper.js
+++ assets/scripts/swiper.js
@@ -1,14 +1,27 @@
 const popularSwiper = new Swiper(".popular-swiper", {
-    slidesPerView: 3,
+    slidesPerView: "auto",
     spaceBetween: 10,
+
+    breakpoints: {
+        1180: {
+            slidesPerView: 3,
+        }
+    }
 })
 
 
 const blogSwiper = new Swiper(".blog__swiper", {
-    slidesPerView: 2,
+    slidesPerView: "auto",
     spaceBetween: 24,
 
-    grid: {
-        rows: 2,
+
+
+    breakpoints: {
+        1060: {
+            grid: {
+                rows: 2,
+            },
+            slidesPerView: 2,
+        }
     }
 })
